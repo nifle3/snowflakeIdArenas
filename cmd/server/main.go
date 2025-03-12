33e1e3c2eb530dcd1b75c5323acc40542c8222d5
nifle3 <kuper116rus.2005@gmail.com>
2025-03-12 11:07:03 +0300

    bat,an

---
+++ cmd/server/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// cfg = config.MustNew()
+	// logger.MustInit()
+	// etcd := etcd.Register()
+	// machineID := etcd.GetMachineId()
+	// snowflakeService = snowFlake.NewService()
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nifle3/goarenas-snowflakeClone
+
+go 1.24.0
